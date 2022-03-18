11670f730e1528e72b228abb6c1cc4beaee1da86
Sakthivel2021 <sakthi2720@gmail.com>
2022-03-18 06:55:15 +0530

    accept

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
-dor='123'
-print(dor.isnumeric())
-name='abd'
-print(name.isalpha())
-sdgghhdttdtdtd
+print(5>10)
+print(10>5>2)
+print(10>5<2)
+print(10==20)
+print(10!=20)
+no1,no2,no3=10,20,30
+print(no1==no2==no3
