4c80dccadc0b89ae2cb0dc47d30e8899cce3ee4f
Sakthivel2021 <sakthi2720@gmail.com>
2022-03-19 13:59:40 +0530

    accept

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
-print(5>10)
-print(10>5>2)
-print(10>5<2)
-print(10==20)
-print(10!=20)
-no1,no2,no3=10,20,30
-print(no1==no2==no3)
+no=int(input('enter number'))
+addition=0
+count=0
+while no>0:
+	addition=addition+(no%10)
+	no=no//10
+	count+=1
+else:
+	print(addition)	
+	print(count)
+	
