5c1e66283dd11d46d77e964150d76d0375284aaf
Sakthivel2021 <sakthi2720@gmail.com>
2022-03-31 20:14:33 +0530

    accept

--- first.py
+++ first.py
@@ -1,18 +1,9 @@
 
-q=[78,67,76,89,78,67,56,45,78,90]
-h=[65,45,35,93,97]
-high=len(q) if len(q)>len(h) else len(h)
-small=len(q) if len(q)<len(h) else len(h)
-a=[]
-for i in range(small):
-	a.append(q[i]+h[i])
+intake="aahfckgkuutkgvktkgk"
+l=[]
+for letter in intake:
+	print(letter,end="")
+	if letter not in l:
+		l.append(letter)
 
-i=high-small
-for no in range(i,high):
-	a.append(q[no])
-	
-print(a)	
-			 
-			 
-
-	
+print(l)
