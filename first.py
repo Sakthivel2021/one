6eba52e8c299edd46f7166f9721226cf35a4a4d0
Sakthivel2021 <sakthi2720@gmail.com>
2022-04-25 10:57:06 +0530

    done

--- first.py
+++ first.py
@@ -1,11 +1,5 @@
-def calculate(no1,no2):
-	sum=no1+no2
-	sub=no1-no2
-	mul=no1*no2
-	div=no2/no1
-	return sum,sub,mul,div
-
-no1=12
-no2=21
-
-print(calculate(no1,no2))
+import math
+print(math.e)
+print(math.pi)
+print(math.tau)
+print(math.inf)
