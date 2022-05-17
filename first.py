7a947b703da6296fe77583221854d8739a40fe08
Sakthivel2021 <sakthi2720@gmail.com>
2022-05-17 10:42:36 +0530

    sakthi

--- first.py
+++ first.py
@@ -1,11 +1,11 @@
 s1=(1,2,3,4,5)
 s2=(1,9,3,8,5)
 
-s3=[]
+s3=set()
 for i in s1:
     
     for j in s2:
          if i==j:
-                s3.append(i)
+                s3.add(i)
                 
 print(s3) 
