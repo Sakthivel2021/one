66d30f4176ad363a9746d4451c5e1059ae7f5041
Sakthivel2021 <sakthi2720@gmail.com>
2022-05-17 10:20:09 +0530

    sakthi

--- first.py
+++ first.py
@@ -1,5 +1,11 @@
-import math
-print(math.e)
-print(math.pi)
-print(math.tau)
-print(math.inf)
+s1=(1,2,3,4,5)
+s2=(1,9,3,8,5)
+
+s3=[]
+for i in s1:
+    
+    for j in s2:
+         if i==j:
+                s3.append(i)
+                
+print(s3) 
