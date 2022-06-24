3ff10ee083fb8a45df26845e05a8f545223665f1
Sakthivel2021 <sakthi2720@gmail.com>
2022-06-24 20:18:29 +0530

    apdate

--- first.py
+++ first.py
@@ -1,13 +1,7 @@
-a=[[1,2,5],
-   [3,4,4]]
-b=[[2,4,3],
-   [4,3,4]]
-c=[[0,0,0],
-   [0,0,0]]
-
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        c[i][j]=a[i][j]+b[i][j]
-        
-for k in c:
-    print(k)
+for i in range(5):
+    if i==5:
+        break
+    else:
+        print(i)
+else:
+    print("coderslearing")
