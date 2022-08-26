a00a4fc5b24f864ca5b99ef47f91bdb883a42e2c
Sakthivel2021 <sakthi2720@gmail.com>
2022-08-26 18:18:31 +0530

    yes

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("helo world")
+no1=1
+no2=100
+for no in range(no1,no2):
+    for div in range(2,no):
+        if no%div==0:
+            
+            break
+        div+=1
+    else:
+        print(no,"prime number")
